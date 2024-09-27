2921d1b972b2453a619597ccd2b342218fa802f5
M3fjuLeo <mateuszropek@onet.pl>
2024-09-27 15:22:18 +0200

    first commit

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { login as loginApi } from "../../services/apiAuth";
+
+export function useLogin() {
+  // queryClient - zapisanie w pamieci danych aby nie odswierzało co chwile
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (user) => {
+      queryClient.setQueryData(["user"], user.user);
+      navigate("/dashboard", { replace: true });
+    },
+    onError: (err) => {
+      console.log("ERROR", err);
+      toast.error("provided email or password are incorrect");
+    },
+  });
+
+  return { login, isLoading };
+}
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+export const supabaseUrl = "https://gwidmvgsehebrohxclhe.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3aWRtdmdzZWhlYnJvaHhjbGhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM5OTg0OTUsImV4cCI6MjAzOTU3NDQ5NX0.dw1tYITVF7ivaxe4B2FnbYeY6doVi_r-96tXjk8TRLE";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
